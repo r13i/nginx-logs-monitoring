c5c77e3656fd295ac69cbc6c8ae0b3173e6b174c
Redouane Achouri <achouri.a.r@gmail.com>
2020-06-10 20:57:43 +0200

    Added IP geolocation.
    Added requesting geolocation of IP addresses from remote API.

--- app.py
+++ app.py
@@ -9,8 +9,32 @@ log_format = '(?P<container>[\w\.\-]+)\s+\| (?P<host>[\w\.\-]+) (?P<remote_addr>
              '\[(?P<local_time>.+)\] "(?P<method>[A-Z]+) (?P<request>[\w\.\-\/]+).+" ' \
              '(?P<status>\d{3}) (?P<bytes_sent>\d+) "(?P<http_referer>.+)" "(?P<http_user_agent>.+)"'
 
+url = 'http://ip-api.com/{type}?fields={fields}'
+fields = 'status,message,country,regionName,city,lat,lon,offset,isp,org,proxy,hosting,query'
+
+def get_ip_data(ip, url, batch=True, fields=None):
+    if not ip:
+        return
+
+    if batch:
+        r = requests.post(
+            url.format(type='batch', fields=fields),
+            json = ip)
+
+    else:
+        r = requests.get(
+            url.format(type='json/' + ip, fields=fields)
+        )
+
+    if r and r.status_code == requests.codes.ok:
+        return r.json()
+
+
+
 if __name__ == "__main__":
 
+    pprint(get_ip_data(["208.80.152.201", "8.8.8.8", "24.48.0.1"], url, fields = fields))
+    # pprint(get_ip_data("208.80.152.201", url, fields = fields, batch = False))
 
     # client = docker.from_env()
     # container = client.containers.get('local-nginx-proxy')
